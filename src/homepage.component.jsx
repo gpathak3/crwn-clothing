e3a6d5c18d87b94b5c48040233d75c87725c0354
Girish Pathak <gcp0303@gmail.com>
2019-10-16 00:34:24 +0530

    this is first ecommerce website loading

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss';
+const HomePage = () => (
+
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                 <h1 className='titile'> Hats </h1>
+                 <span className='subtitle'>shop now </span>
+                </div> 
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                 <h1 className='titile'> jackets </h1>
+                 <span className='subtitle'>shop now </span>
+                </div> 
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                 <h1 className='titile'> sneakers </h1>
+                 <span className='subtitle'>shop now </span>
+                </div> 
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                 <h1 className='titile'> men </h1>
+                 <span className='subtitle'>shop now </span>
+                </div> 
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                 <h1 className='titile'> women </h1>
+                 <span className='subtitle'>shop now </span>
+                </div> 
+            </div>
+        </div>
+    </div>
+ )
+
+export default HomePage;
